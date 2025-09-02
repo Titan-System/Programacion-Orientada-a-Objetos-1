3bc385c7846f7956864d711c6a85a4c335c3e161
Repler_# <145279061+LucianoCespedes@users.noreply.github.com>
2025-09-02 17:09:28 -0300

    Add files via upload

---
+++ Luciano/java/basicos/introduccion.java
@@ -0,0 +1,216 @@
+package java.basicos;
+import java.util.Scanner;
+
+public class introduccion {
+
+    public static void main(String[] args) {
+
+
+        // String palabra;
+        // Scanner input = new Scanner(System.in);
+
+        // System.out.println("introdusca una palabra");
+        // palabra=input.nextLine();
+        // System.out.println("la palabra es "+palabra);
+
+
+        // String nombre="hola mundo";
+
+        // System.out.println("La variable dice "+nombre);
+        // int a=3;
+        // int b=2;
+        // int c=5;
+        // double resultado = 0.0;
+
+        // resultado=a+b/b+c;
+
+
+        // System.out.println(Math.pow(resultado, b));
+        
+///////////////////////////////////////////////////////////////////////////
+        // int valor=0;
+        // int suma=0;
+
+        // Scanner input =new Scanner(System.in);
+
+        // System.out.println("Introdusca el primer valor " );
+        // valor=input.nextInt();
+
+        // suma=valor;
+        // valor=0;
+
+        // System.out.println("Introdusca el segundo valor " );
+        // valor=input.nextInt();
+
+        // suma+=valor;
+
+        // System.out.println("El resultado de la suma es "+ suma);
+///////////////////////////////////////////////////////////////////////////
+        // int pagohoras=0;
+        // int horastrabajadas=0;
+        // int pago=0;
+        // Scanner input1 =new Scanner(System.in);
+        // Scanner input2 =new Scanner(System.in);
+
+        // System.out.println("Ingrese la cantidad de horas trabajadas ");
+        // horastrabajadas=input1.nextInt();
+
+        // System.out.println("Ingrese el pago por horas correspondiente ");
+        // pagohoras=input2.nextInt();
+
+        // pago=pagohoras*horastrabajadas;
+
+        // System.out.println("Le corresponden "+"$ "+pago);
+///////////////////////////////////////////////////////////////////////////
+       
+        // int numero=0;
+        // int suma=0;
+        // int dividendo=0;
+
+        // Scanner imput =new Scanner(System.in);
+
+        // System.out.println("Ingrese su primer numero: ");
+        // numero=imput.nextInt();
+
+        // suma=numero;
+        // numero=0;
+
+        // System.out.println("Ingrese su segundo numero: ");
+        // numero=imput.nextInt();
+
+        // dividendo=suma/numero;
+        // suma+=numero;
+        // numero=0;
+
+        // System.out.println("La suma de suma es: "+suma);
+        // System.out.println("La suma de suma es: "+dividendo);
+
+/////////////////////////////////////////////////////////////////////////        
+
+        // int numero=0;
+        // int j=0;
+
+        // Scanner input = new Scanner(System.in);
+
+
+        // System.out.println("Ingrese el cantidad de veces a sumar: ");
+        // j = input.nextInt();
+
+        // System.out.println("//////");
+
+        // for(int i=0; i < j; i++)
+        // {
+        //         numero+=1;
+        //         System.out.println(numero);
+        // }
+////////////////////////////////////////////////
+
+        // double ICM=0;
+        // double cm=0;
+        // int Kg=0;
+
+        // Scanner input= new Scanner(System.in);
+
+        // System.out.println("Ingrese su Peso en kg : ");
+        // Kg = input.nextInt();
+
+        // System.out.println("Ingrese su altura en m : ");
+        // cm = input.nextDouble();
+
+        // ICM= Kg / cm * cm;
+        // System.out.println("Tu indice de masa corporal es: "+ ICM);
+
+//////////////////////////////////
+        
+        // int N=0;
+        // int M=0;
+
+        // Scanner input=new Scanner(System.in);
+
+        // System.out.println("Ingrese el primer numero: ");
+        // N =input.nextInt();
+
+        // System.out.println("Ingrese el segundo numero: ");
+        // M =input.nextInt();
+
+        // System.out.println("El cociente es : "+ N/M);
+        // System.out.println("El resto es: "+N%M);
+
+//////////////////////////////////
+        
+        // int inversion;
+        // int interes_anual;
+        // int tiempo;
+
+        // Scanner input = new Scanner(System.in);
+
+        // System.out.println("Ingrese el monto a invertir: ");
+        // inversion = input.nextInt();
+
+        // System.out.println("Ingrese el interes anual: ");
+        // interes_anual = input.nextInt();
+
+        // System.out.println("Ingrese la cantidad de tiempo: ");
+        // tiempo = input.nextInt();
+
+
+        // System.out.println("El retorno de la inversion es: "+(inversion*interes_anual*tiempo)/100);
+//////////////////////////////////
+
+        // String N;
+        // int peso_autos = 112;
+        // int peso_munecas= 75;
+        // int contador_1=0;
+        // int contador_2=0;
+        // int vueltas=0;
+        // int flag=0;
+        // int compra=0;
+
+        // Scanner input =new Scanner(System.in);
+
+        // System.out.println("Bienvenido Precione Y para comenzar: ");
+        // N = input.nextLine();
+
+        // while (!N.equals("n")) {
+        //        vueltas+=1; 
+        //         System.out.println("1- Autos / 2- Munieca/n");
+        //         System.out.println("Que desea comprar: ");
+        //         flag= input.nextInt();
+                
+        //         input.nextLine();
+        //         switch (flag) {
+        //                 case 1:
+        //                         contador_1+=1;
+        //                         compra+=peso_autos;
+
+        //                 break;
+        //                 case 2:
+        //                         contador_2+=1;
+        //                         compra+=peso_munecas;
+
+        //                 break;
+
+        //                 default:
+        //                         break;
+        //         }
+
+        
+        //         System.out.println("seguir con la compra: ");
+        //         N = input.nextLine();
+
+        // }
+
+
+        // System.out.println("El peso todal es: "+ compra);
+        // System.out.println("La cantidad de autos es: "+ contador_1);
+        // System.out.println("La cantidad de muniecas es: "+ contador_2);
+
+        // input.close();
+//////////////////////////////////        
+
+
+
+
+        }
+
+}
